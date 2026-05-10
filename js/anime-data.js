86856ce14de8dba661cf3ec48b48365910dec714
Asdevil927 <aniketsangashetti110@gmail.com>
2026-05-10 20:43:10 +0530

    Create js/anime-data.js

---
+++ js/anime-data.js
@@ -0,0 +1,31 @@
+const animeList = [
+  {
+    id: 1,
+    title: "Naruto",
+    category: "action",
+    image: "https://via.placeholder.com/300x400",
+    description: "A ninja anime.",
+    episodes: 10,
+    video: "https://www.youtube.com/embed/dQw4w9WgXcQ"
+  },
+
+  {
+    id: 2,
+    title: "Attack on Titan",
+    category: "action",
+    image: "https://via.placeholder.com/300x400",
+    description: "Humans vs Titans.",
+    episodes: 12,
+    video: "https://www.youtube.com/embed/dQw4w9WgXcQ"
+  },
+
+  {
+    id: 3,
+    title: "Your Name",
+    category: "romance",
+    image: "https://via.placeholder.com/300x400",
+    description: "Romantic anime movie.",
+    episodes: 1,
+    video: "https://www.youtube.com/embed/dQw4w9WgXcQ"
+  }
+];
