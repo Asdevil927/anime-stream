8accb0d5d04c4fb5f9958e11620a61017779dbb0
Asdevil927 <aniketsangashetti110@gmail.com>
2026-05-10 20:46:46 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
+const trendingGrid = document.getElementById("trendingGrid");
+const popularGrid = document.getElementById("popularGrid");
+
+function createCards() {
+
+  animeList.forEach(anime => {
+
+    const card = `
+      <div class="card" data-category="${anime.category}">
+        <img src="${anime.image}" alt="${anime.title}">
+        <h3>${anime.title}</h3>
+      </div>
+    `;
+
+    trendingGrid.innerHTML += card;
+    popularGrid.innerHTML += card;
+  });
+}
+
+createCards();
+
 document.querySelectorAll(".category-btn").forEach(button => {
+
   button.addEventListener("click", () => {
+
     const category = button.dataset.category;
 
     document.querySelectorAll(".card").forEach(card => {
+
       if (
         category === "all" ||
         card.dataset.category === category
@@ -11,6 +35,9 @@ document.querySelectorAll(".category-btn").forEach(button => {
       } else {
         card.style.display = "none";
       }
+
     });
+
   });
+
 });
